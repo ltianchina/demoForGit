fa314461963ab705699a7066b23dd654505ddff6
ltianchina <ltian@line6.com>
2014-09-28 16:22:21 +0800

    first commit

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirTurnExample
+//
+//  Created by Nick Brook on 02/01/2012.
+//  Copyright (c) 2012 Nick Brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
